daa775d337f6af89e6812a8f51f2a9cb4f4341a7
Andrea Angelova <Andrea.Angelova@opengi.mk>
2022-02-02 19:23:45 +0100

    add event listeners

--- G1/Class05 - JediArchives/jediArchives/src/app.js
+++ G1/Class05 - JediArchives/jediArchives/src/app.js
@@ -1,1 +1,23 @@
-console.log("connected")
+var peopleBtn = document.getElementById("peopleBtn");
+var shipsBtn = document.getElementById("shipsBtn");
+var tableHeader = document.getElementsByTagName("thead")[0];
+
+var peopleHeader = ["Name", "Height ( cm )", "Mass ( kg )", "Gender", "Birth Year", "Appearances"];
+var shipsHeader = ["Name", "Model", "Manufacturer", "Cost", "People Capacity", "Class"];
+
+peopleBtn.addEventListener("click", function() {
+    generateTableHeader(peopleHeader);
+});
+
+
+shipsBtn.addEventListener("click", function() {
+    generateTableHeader(shipsHeader);
+});
+
+function generateTableHeader(values) {
+    var html = "";
+    for(let value of values) {
+        html += `<th>${value}</th>`
+    }
+    tableHeader.innerHTML = html;
+}
