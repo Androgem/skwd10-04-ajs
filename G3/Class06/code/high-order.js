5e2ed8132094e87455c3a4063a4afcd62c556e18
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2022-02-07 20:00:44 +0100

    Class06 part-03

--- G3/Class06/code/high-order.js
+++ G3/Class06/code/high-order.js
@@ -6,7 +6,7 @@ let students = [
     { firstName: "Lucky", lastName: "J", grade: 5, age: 18 },
     { firstName: "Strike", lastName: "K", grade: 4, age: 16 },
     { firstName: "Eric", lastName: "I", grade: 1, age: 17 }
-]
+];
 let students2 = [
     {
         firstName: "Bob", lastName: "H", grades: [
@@ -20,4 +20,97 @@ let students2 = [
             { subject: "Science", grade: 5 }
         ], age: 33
     }
-]
+];
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(`${students[i].firstName} ${students[i].lastName}`);
+// }
+
+// students.forEach(function (student) {
+//     console.log(`mr. ${student.firstName} ${student.lastName}`);
+// });
+
+//students.forEach(student => console.log(`dr. ${student.firstName} ${student.lastName}`));
+
+let studentWithGrade5 = [];
+for (let student of students) {
+    if (student.grade === 5) {
+        studentWithGrade5.push(student);
+    }
+}
+//studentWithGrade5.forEach(student => console.log(`${student.firstName} ${student.lastName} ${student.age}`));
+
+
+// students
+//     .filter(student => student.grade === 5)
+//     .forEach(student => console.log(`${student.firstName} ${student.lastName} ${student.age}`));
+
+let std = students
+    .filter(student => student.age >= 18)
+    .filter(student => student.grade > 4);
+
+// console.log(std);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 2, 2, 1, 2];
+let onlyTwos = numbers.filter(num => num === 2);
+let onlyTwos1 = numbers.filter(num => true); // all the elements will be returned
+// console.log(onlyTwos);
+
+
+let mappedStudents = [];
+for (let student of students) {
+    let fullName = `${student.firstName} ${student.lastName}`;
+    let dob = new Date().getFullYear() - student.age;
+    mappedStudents.push({
+        fullName: fullName,
+        dateOfBirth: dob
+    });
+}
+// console.log(mappedStudents);
+
+let newStudentsArray = students.map(student => {
+    let fullName = `${student.firstName} ${student.lastName}`;
+    let dob = new Date().getFullYear() - student.age;
+    return {
+        fullName: fullName,
+        dateOfBirth: dob
+    };
+});
+// console.log(newStudentsArray);
+
+let grades = students.map(student => student.grade);
+// console.log(grades);
+
+let numbers1 = [1, 2, 3, 4, 5, 6];
+let numbersMultiply = numbers1.map(x => x * 2);
+// console.log(numbersMultiply);
+
+
+
+
+let numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 2, 2, 1, 2];
+
+let sum = 0;
+for (let num of numbers2) {
+    sum += num;
+}
+console.log(sum);
+
+let reduceSum = numbers2.reduce((prev, curr) => prev += curr, 0);
+console.log(reduceSum);
+
+let sumGrades = students.reduce((secondReduceParameter, student) => secondReduceParameter += student.grade, 0); // secondReduceParameter = 0 the zero after the comma
+console.log(sumGrades);
+
+let someObj = students.reduce((prev, curr) => {
+    prev[curr.firstName] = curr.age;
+    return prev;
+}, {})
+console.log(someObj);
+
+let someArr = students.reduce((prev, curr) => {
+    prev.push(`${curr.firstName} ${curr.lastName}`);
+    return prev;
+}, []);
+
+console.log(someArr);
