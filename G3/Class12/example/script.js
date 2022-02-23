c942a5a31cfa00b4958914384eb03a73a8bdfc9b
Trajan Stevkovski <trajan.stevkovski@seavus.com>
2022-02-23 18:39:53 +0100

    Class12 part-1

---
+++ G3/Class12/example/script.js
@@ -0,0 +1,24 @@
+let stopWatch = {
+    time: 0,
+    interval: null,
+
+    start: function () {
+        this.interval = setInterval(() => {
+            this.time++;
+            console.log(this.time);
+        }, 1000);
+    },
+
+    stop: function () {
+        clearInterval(this.interval);
+        this.interval = null;
+    },
+
+    reset: function () {
+        if (this.interval) {
+            clearInterval(this.interval);
+            this.interval = null;
+        } 
+        this.time = 0;
+    }
+};
