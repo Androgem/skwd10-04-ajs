a72a1b4f16b17554480205bdbb03725dada08771
chetelev <chetelev@gmail.com>
2022-03-02 19:34:23 +0100

    weather app requirements

--- G1/Class14 - WeatherAlert/code/src/app.js
+++ G1/Class14 - WeatherAlert/code/src/app.js
@@ -22,6 +22,12 @@ const navService = {
         navService.showPage(navService.pages[i]);
       });
     }
+    this.searchButton.addEventListener('click', function (e) {
+      e.preventDefault();
+      uiService.tbody.innerHTML = '';
+      weatherService.city = navService.navSearch.value;
+      weatherService.getWeather();
+    });
   },
 };
 
@@ -37,6 +43,9 @@ const weatherService = {
       .then((res) => res.json())
       .then((res) => {
         uiService.renderStatistics(res);
+        res.list.forEach((res) => {
+          uiService.generateTable(res);
+        });
       })
       .catch((err) => console.log(err));
   },
@@ -78,7 +87,8 @@ const weatherService = {
 
 const uiService = {
   statisticsResult: document.getElementById('statisticsResult'),
-  createGrid: function (data) {
+  tbody: document.getElementById('tbody'),
+  generateGrid: function (data) {
     const row = document.createElement('div');
     const col = document.createElement('div');
     row.classList.add('row');
@@ -87,48 +97,71 @@ const uiService = {
     row.appendChild(col);
     return row;
   },
+  generateTable: function (data) {
+    const tr = document.createElement('tr');
+    this.tbody.appendChild(tr);
+    tr.innerHTML = this.fillTable(data);
+  },
+  fillTable: function (time) {
+    const url = 'http://openweathermap.org/img/w/';
+    return `
+    <td> <img src="${url + time.weather[0].icon}.png"> </td>
+    <td>${time.weather[0].description}</td>
+    <td>${time.dt_txt}</td>
+    <td>${time.main.temp}</td>
+    <td>${time.main.humidity}</td>
+    <td>${time.wind.speed}</td>
+    `;
+  },
   renderStatistics: function (weatherData) {
+    this.clearElement(this.statisticsResult);
     const statistics = weatherService.aggregateStatistics(weatherData);
     this.statisticsResult.appendChild(
-      this.createGrid(`City: ${weatherData.city.name}`)
+      this.generateGrid(`City: ${weatherData.city.name}`)
     );
 
     this.statisticsResult.appendChild(
-      this.createGrid(`Temperature highest: ${statistics.temperature.highest}`)
+      this.generateGrid(
+        `Temperature highest: ${statistics.temperature.highest}`
+      )
     );
 
     this.statisticsResult.appendChild(
-      this.createGrid(`Temperature lowest: ${statistics.temperature.lowest}`)
+      this.generateGrid(`Temperature lowest: ${statistics.temperature.lowest}`)
     );
 
     this.statisticsResult.appendChild(
-      this.createGrid(`Temperature average: ${statistics.temperature.average}`)
-    );
-
-    console.log(statistics);
-    this.statisticsResult.appendChild(
-      this.createGrid(`Humidity highest: ${statistics.humidity.highest}`)
+      this.generateGrid(
+        `Temperature average: ${statistics.temperature.average}`
+      )
     );
 
     this.statisticsResult.appendChild(
-      this.createGrid(`Humidity lowest: ${statistics.humidity.lowest}`)
+      this.generateGrid(`Humidity highest: ${statistics.humidity.highest}`)
     );
 
     this.statisticsResult.appendChild(
-      this.createGrid(`Humidity average: ${statistics.humidity.average}`)
+      this.generateGrid(`Humidity lowest: ${statistics.humidity.lowest}`)
     );
 
     this.statisticsResult.appendChild(
-      this.createGrid(
+      this.generateGrid(`Humidity average: ${statistics.humidity.average}`)
+    );
+
+    this.statisticsResult.appendChild(
+      this.generateGrid(
         `Wormest time of the days ahead: ${statistics.warmestTime}`
       )
     );
 
     this.statisticsResult.appendChild(
-      this.createGrid(
+      this.generateGrid(
         `Coldest time of the days ahead: ${statistics.coldestTime}`
       )
     );
+  },
+  clearElement(element) {
+    element.innerHTML = '';
   },
 };
 
